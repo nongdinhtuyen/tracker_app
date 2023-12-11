9b17a5a0fe53e7626d89f706dfd9b20d479c22ec
tuyennd <tuyennd@rocketstudio.com.vn>
2023-12-11 16:05:07 +0700

    init

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+import 'package:tracker_app/widgets/expenses.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      theme: ThemeData(useMaterial3: true),
+      home: const Expenses(),
+    ),
+  );
+}
